7256ff943812b60a0c2925d57eefeacaa05c2859
Zmoore <zmoore371@gmail.com>
2022-01-04 18:15:00 -0500

    updated styling for work page, added screenshots of apps. Need to update apps to newer projects.

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -11,35 +11,42 @@ export default function Work() {
                       <a className="repo-link" href="https://github.com/PhilipBrowning/project1">Link to Repository</a>
                     </div>
                 </a>
-                {/* <a className="repo-link" href="https://github.com/PhilipBrowning/project1">Link to Repository</a> */}
-                <a href="https://zmoore371.github.io/Homework-Week-6/" className="large-card" id="card2">
+
+                <a href="https://lit-coast-69358.herokuapp.com/" className='large-card' id="card2">
+                    <div className='label'>
+                        <h3>Review Website</h3>
+                        <a className='repo-link' href="https://github.com/zmoore371/Project-2">Link to Repository</a>
+                    </div>
+                </a>
+    
+                <a href="https://zmoore371.github.io/Homework-Week-6/" className="large-card" id="card3">
                     <div className="label">
                       <h3>Weather Dashboard</h3>
                       <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-6">Link to Repository</a>
                     </div>
                 </a>
-                {/* <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-6">Link to Repository</a> */}
-                <a href="https://zmoore371.github.io/Homework-Week-5/" className="large-card" id="card3">
+                
+                <a href="https://zmoore371.github.io/Homework-Week-5/" className="large-card" id="card4">
                     <div className="label">
                       <h3>Day Planner</h3>
                       <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-5">Link to Repository</a>
                     </div>
                 </a>
-                {/* <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-5">Link to Repository</a> */}
-                <a href="https://zmoore371.github.io/Homework-Week-4/" className="large-card" id="card4">
+                
+                <a href="https://zmoore371.github.io/Homework-Week-4/" className="large-card" id="card5">
                     <div className="label">
                       <h3>Javascript Quiz</h3>
                       <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-4">Link to Repository</a>
                     </div>
                 </a>
-                {/* <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-4">Link to Repository</a> */}
-                <a href="https://zmoore371.github.io/Homework-Week-3/" className="large-card" id="card5">
+                
+                <a href="https://zmoore371.github.io/Homework-Week-3/" className="large-card" id="card6">
                     <div className="label">
                       <h3>Password Generator</h3>
                       <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-3">Link to Repository</a>
                     </div>
                 </a>  
-                {/* <a className="repo-link" href="https://github.com/zmoore371/Homework-Week-3">Link to Repository</a>           */}
+                
             </div>
         </div>
     )
